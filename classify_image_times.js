65479b7d2f508cc50e77f5789ac77e5e655b213f
Michele Toni <michele.toni@cynny.com>
2017-08-02 11:10:58 +0200

    HTML and JS for times evaluation

---
+++ classify_image_times.js
@@ -0,0 +1,136 @@
+
+function logProgress(progress) {
+  $('#myProgress')
+        .css('width', progress+'%')
+        .attr('aria-valuenow', progress);
+}
+
+function resetProgress() {
+  $('#myProgress')
+        .attr('class', 'progress-bar')
+        .css('width', '0%')
+        .attr('aria-valuenow', '0')
+        .html('');
+}
+
+function logEvent(str) {
+  console.log(str);
+  var d = document.createElement('div');
+  d.innerHTML = str;
+  document.getElementById('result').appendChild(d);
+}
+
+function logError(message) {
+  $('#myProgress')
+        .attr('class', 'progress-bar progress-bar-danger')
+        .css('width', '100%')
+        .attr('aria-valuenow', 100).html(message);
+  logEvent(message);
+}
+
+
+function preproc(url, targetLen, meanimg, callback) {
+  var canvas = document.getElementById('myCanvas');
+  var context = canvas.getContext('2d');
+  var image = new Image();
+  var targetLen = 224;
+  image.setAttribute('crossOrigin', 'anonymous');
+  image.onload = function() {
+    var sourceWidth = this.width;
+    var sourceHeight = this.height;
+    var shortEdge = Math.min(this.width, this.height);
+    var yy = Math.floor((sourceHeight - shortEdge) / 2);
+    var xx = Math.floor((sourceWidth - shortEdge) / 2);
+    context.drawImage(image,
+                      xx, yy,
+                      shortEdge, shortEdge,
+                      0, 0,
+                      targetLen, targetLen);
+    var imgdata = context.getImageData(0, 0, targetLen, targetLen);
+    var data = new Float32Array(targetLen * targetLen * 3);
+    var stride = targetLen * targetLen;
+    for (var i = 0; i < stride; ++i) {
+      data[stride * 0 + i] = imgdata.data[i * 4 + 0];
+      data[stride * 1 + i] = imgdata.data[i * 4 + 1];
+      data[stride * 2 + i] = imgdata.data[i * 4 + 2];
+    }
+    if (typeof meanimg !== 'undefined') {
+      for (var i = 0; i < data.length; ++i) {
+        data[i] = data[i] - meanimg.data[i];
+      }
+    } else {
+      // use 117 as mean by default.
+      for (var i = 0; i < data.length; ++i) {
+        data[i] = data[i] - 117;
+      }
+    }
+    var nd = ndarray(data, [1, 3, targetLen, targetLen]);
+    callback(nd);
+  };
+  $(image).bind('error', function (event) {
+    logError("Opps.. Failed to load image " + url);
+  });
+  image.src = url;
+}
+
+function start() {
+  var modelUrl = "./model/squeezenet-model.json";
+  logEvent("Loading " + modelUrl);
+   $.getJSON(modelUrl, function(model) {
+       var url = document.getElementById("imageURL").value;
+       pred = new Predictor(model, {'data': [1, 3, 224, 224]});
+       preproc(url, 224, pred.meanimg,  function(nd) {
+           pred.setinput('data', nd);
+           logEvent("start... prediction... this can take a while");
+           // delay 1sec before running prediction, so the log event renders on webpage.
+           var start = performance.now();
+           // print every 10%
+           var print_step = 10;
+           // reset progress bar
+           resetProgress();
+
+           function trainloop(ops, step, nleft, next_goal, finish_callback) {
+               if (nleft == 0) {
+                 finish_callback(); return;
+               }
+               var beforeOp = performance.now();
+               nleft = pred.partialforward(step);
+               var afterOp = performance.now();
+               var opName = ops[step].name;
+               //No parameters loading
+               if (opName.indexOf("weight") == -1 && opName.indexOf("bias") == -1) {
+                  logEvent(opName + ": " + (afterOp-beforeOp).toFixed(3));
+               }
+               progress = (step + 1) / (nleft + step + 1) * 100;
+               if (progress >= next_goal || progress == 100) {
+                   logProgress(progress);
+                   setTimeout(function() {
+                       trainloop(ops, step + 1, nleft, next_goal + print_step, finish_callback);
+                   }, 1);
+               } else {
+                   setTimeout(function() {
+                       trainloop(ops, step + 1, nleft, next_goal, finish_callback);
+                   }, 0);
+               }
+           }
+           trainloop(model.symbol.nodes, 0, 1, 0, function() {
+              logEvent("finished prediction....");
+              out = pred.output(0);
+              var index = new Array();
+              for (var i=0;i<out.data.length;i++) {
+                index[i] = i;
+              }
+              max_output = Number(document.getElementById("max-output").value);
+              logEvent("Max output = " + max_output);
+              index.sort(function(a,b) {return out.data[b]-out.data[a];});
+              var end = performance.now();
+              var time = ((end - start) / 1000).toFixed(3);
+              logEvent("time-cost=" + time + " sec");
+              for (var i = 0; i < max_output; i++) {
+                logEvent('Top-' + (i+1) + ':' + model.synset[index[i]] + ', value=' + out.data[index[i]]);
+              }
+              pred.destroy();
+           });
+       });
+   });
+}
